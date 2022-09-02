2d75a03b87b821007a2293f87cd8f999dffdf2a5
Huu Lam Nguyen <bobaguy@Huus-MBP-2.localdomain>
2022-09-02 15:15:18 -0400

    Sec 11 User model and validate email

---
+++ models/User.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+        minlength: 3,
+        maxlength: 20,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email" ],
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: "Please provide  a valid email"
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 6
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        maxlength: 20,
+        default: "LastName"
+    }, 
+    location: {
+        type: String,
+        maxlength: 20,
+        trim: true,
+        default: "my city"
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
