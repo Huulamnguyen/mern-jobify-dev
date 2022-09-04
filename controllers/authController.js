4e89dd099c04ce11d064937f16b12de6b6f6d493
Huu Lam Nguyen <liamnguyen.swe@gmail.com>
2022-09-04 00:43:21 -0400

    Sec 15 jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,10 +13,22 @@ const register = async (req, res) => {
     const userAlreadyExists = await User.findOne({email})
     if(userAlreadyExists){
         throw new BadRequestError("Email already in use")
-    }
+    } 
 
     const user = await User.create({name, email, password})
-    res.status(StatusCodes.CREATED).json({user})
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED)
+        .json({
+            user: 
+                {
+                    name: user.name,
+                    lastName: user.lastName,
+                    email: user.email,
+                    location: user.location,
+                }, 
+            token,
+            location: user.location
+        })
 }
 
 const login = (req, res) => {
