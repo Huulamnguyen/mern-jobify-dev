6291b2a9e63e243e54f6df5fd7c056ef2802351a
Huu Lam Nguyen <liamnguyen.swe@gmail.com>
2022-09-20 16:40:50 -0400

    Section 28 Axios Setup

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -33,6 +33,40 @@ const AppProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(reducer, initialState)
 
+    // axios global setup
+    // axios.defaults.headers.common['Authorization'] = `Bearer ${state.token}`
+
+    // axios custom instance
+    const authFetch = axios.create({
+        baseURL: '/api/v1'
+    })
+
+    // axios add a request interceptor
+    authFetch.interceptors.request.use(
+        (config) => {
+            config.headers.common['Authorization'] = `Bearer ${state.token}`
+            return config
+        },
+        (error) => {
+            return Promise.reject(error)
+        }
+    )
+
+    // axios add a response interceptor
+    authFetch.interceptors.response.use(
+        (response) => {
+            return response
+        },
+        (error) => {
+            console.log(error.response)
+            if (error.response.status === 401) {
+            console.log('AUTH ERROR')
+            }
+            return Promise.reject(error)
+        }
+    )
+
+
     const displayAlert = () => {
         dispatch({type: DISPLAY_ALERT});
         clearAlert();
@@ -87,14 +121,10 @@ const AppProvider = ({ children }) => {
 
     const updateUser = async (currentUser) => {
         try {
-            const { data } = await axios.patch('/api/v1/auth/updateUser', currentUser, {
-                headers:{
-                    Authorization: `Bearer ${token}`,
-                }
-            })
+            const { data } = await authFetch.patch('/auth/updateUser', currentUser)
             console.log(data)
         } catch (error) {
-            console.log(error.response)
+            // console.log(error.response)
         }
     }
 
