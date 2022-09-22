50e1f55e7fe56784cb59c86ead7836606adcb8a6
Huu Lam Nguyen <liamnguyen.swe@gmail.com>
2022-09-22 18:03:14 -0400

    Sec 32 Get All Jobs - Server

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -16,19 +16,23 @@ const createJob = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ job })
 }
 
-const deleteJob = (req, res) => {
+
+const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({ createdBy: req.user.userId })
+    res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+}
+
+const deleteJob = async (req, res) => {
     res.send("Delete Job")
 }
 
-const getAllJobs = (req, res) => {
-    res.send("Get All Jobs")
-}
-
-const updateJob = (req, res) => {
+const updateJob = async (req, res) => {
     res.send("Update Job")
 }
 
-const showStats = (req, res) => {
+const showStats = async (req, res) => {
     res.send("Show Stats Job")
 }
 
